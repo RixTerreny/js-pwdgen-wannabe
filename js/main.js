1a2252d78bf19a22eda249883756ff6e3608aca3
RixTerreny <riccardotrave43@gmail.com>
2022-10-26 21:34:24 +0200

    upgraded version

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 const nome = prompt("inserisci nome");
+const upper = nome.charAt(0).toUpperCase() + nome.slice(1)
 const cognome = prompt("inserisci cognome");
-const colore = prompt("inserisci coloreds");
+const colore = prompt("inserisci colore");
 
-const fullInfo = nome + cognome + colore;
+const fullInfo = upper + cognome + colore;
 
-const h1Saluto = document.getElementById("saluto");
+const h2Password = document.getElementById("password");
 
-h1Saluto.innerHTML = fullInfo;
+h2Password.innerHTML = fullInfo + "." +21;
